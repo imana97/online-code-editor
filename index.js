b1c04c9b003152f4995b98d6932d2ff6d4a9ea98
Iman Khaghani Far <iman.khaghani@gmail.com>
2018-05-11 18:33:00 -0400

    update url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var api = new ParseServer({
     databaseURI: 'mongodb://localhost:27017/onlineCodeEditor', // Connection string for your MongoDB database
     appId: 'myAppId',
     masterKey: 'EghjhdMSZkujqkodhSQ5rRZLqHmuUbg7zOnu86zNJalsdfk43lf', // Keep this key secret!
-    serverURL: 'https://calocode.com/parse' // Don't forget to change to https if needed
+    serverURL: 'http://online-code-editor.calocode.com/parse' // Don't forget to change to https if needed
 });
 
 // Serve the Parse API on the /parse URL prefix
